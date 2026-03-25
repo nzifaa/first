cf9a3311f278e2ba8e5bf3a1246894fdc2d063f6
Nzfa <nzifaizz@gmail.com>
2026-03-25 14:54:10 +0700

    ubah foto untuk Person1

--- src/pages/Person1.jsx
+++ src/pages/Person1.jsx
@@ -1,4 +1,4 @@
-import foto from '../assets/foto.jpg'
+import foto from 'https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_640.png'
 export default function Person1() {
   return (
     <div className="p-6">
